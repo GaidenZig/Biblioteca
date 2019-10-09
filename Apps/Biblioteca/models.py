70cc3ae79a280dcddbb0942ae111188cefe59580
Diego Sandon <diegolast106@gmail.com>
2019-10-08 23:39:10 -0300

    cambio necesario en el modelo, para futura funcion en el administrador y también para facilitar las querys necesarias en index

--- Apps/Biblioteca/models.py
+++ Apps/Biblioteca/models.py
@@ -14,6 +14,7 @@ class Autor(models.Model):
 
 class Genero(models.Model):
     nombre = models.CharField(max_length=200, help_text="Ingrese el nombre del género (p. ej. Ciencia Ficción, Poesía Francesa etc.)")
+    activo = models.BooleanField(default=False)
     
     def __str__(self):
         return self.nombre
