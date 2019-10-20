e68dbaf10fa7bde2a4ce643be9143bcef46ad7b3
matias54321 <54971512+matias54321@users.noreply.github.com>
2019-10-20 01:03:39 -0300

    queryset para buscar en la galeria

--- Apps/Biblioteca/urls.py
+++ Apps/Biblioteca/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import crearAutor,cargarLibro,listarAutor
+from .views import crearAutor,cargarLibro,listarAutor,editarAutor,eliminarAutor
 
 urlpatterns=[
     path('crear_autor/', crearAutor,name='crear_autor'),
     path('listar_autor/', listarAutor,name='listar_autor'),
-    path('Libro/',cargarLibro,name='libro')
+    path('Libro/',cargarLibro,name='libro'),
+    path('editar_autor/<int:id>/', editarAutor,name='editar_autor'),
+    path('eliminar_autor/<int:id>/', eliminarAutor,name='eliminar_autor'),
 ]
--- Apps/Biblioteca/views.py
+++ Apps/Biblioteca/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import AutorForm
 from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from sjango.core.paginator import Paginator
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
@@ -10,11 +13,16 @@ from Apps.usuarios.models import MyUser
 from .models import Genero, Libro,Autor
 
 # Create your views here.
-def Home(request):    
+def Home(request):   
+    queryset=request.GET.get("Buscar") 
     current_user=request.user   
     top_libros=Libro.objects.filter(puntuacion__gt=4)
+    if queryset:
+        libros=Libro.objects.filter(Q(titulo__icontains=queryset))
+        return render(request,'Biblioteca/galeria.html',{'libros':libros})
     return render(request,'index.html',{'user':current_user,'top':top_libros})
-    
+
+
 def crearAutor(request):
     if request.method == 'POST':
         print(request.POST)
@@ -30,12 +38,36 @@ def listarAutor(request):
     autores=Autor.objects.all()
     return render(request,'Biblioteca/listar_autor.html',{'autores':autores})
 
-def cargarLibro(request):
-    print(request.GET)
+def editarAutor(request,id):
+    autor_form=None
+    error=None
+    try:
+        autor=Autor.objects.get(id = id)
+        if request.method =='GET':
+            autor_form=AutorForm(instance = autor)
+        else:
+            autor_form=AutorForm(request.POST,instance=autor)
+            if autor_form.is_valid():
+                autor_form.save()
+            return redirect('listar_autor')
+    except ObjectDoesNotExist as e:
+        error=e   
+    return render(request,'Biblioteca/editar_autor.html',{'autor_form':autor_form,'error':error})
 
+def eliminarAutor(request,id):
+    autor=Autor.objects.get(id=id)
+    autor.delete()
+    return redirect('Biblioteca:listar_autor')
+   
+def cargarLibro(request):
+    print(request.GET) 
 
 def galeria(request):
-    libros=Libro.objects.all()
+    queryset=request.GET.get("Buscar")
+    libros=Libro.objects.all() 
+    if queryset:
+        libros=Libro.objects.filter(Q(titulo__icontains=queryset))
+    paginate_by=3
     return render(request,'Biblioteca/galeria.html',{'libros':libros})
 
 def perfil(request):    
