ee3e80a92c35ae2b3ed2583ef845977df1948304
matias54321 <54971512+matias54321@users.noreply.github.com>
2019-12-03 07:04:52 -0300

    ultimo commit

--- Apps/Biblioteca/static/Js/Parallax.js
+++ Apps/Biblioteca/static/Js/Parallax.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     var ancho = $(window).width();    
     if (ancho <= 1350){
         $('html').css({
-            'background-size': 'initial'
+            'background-size': 'cover'
         });
     }
 
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
-		var posicion = barra * 0.15;
+		var posicion = barra * 0.10;
 
 		$('html').css({
 			'background-position': '0 -' + posicion + 'px'
